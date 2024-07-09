b84cfd0002c3b2dccb6e815f2cc837f565ba514a
Diogo Carreira <dcweb19@gmail.com>
2024-07-09 20:11:29 +0100

    exercicio_1

---
+++ List_patterns.py
@@ -0,0 +1,8 @@
+#fix this code so that it prints a sorted list of all of your friends (alphabetical)
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+friends.extend(new_friend)
+
+print(sorted(friends))
