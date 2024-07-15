dc4430459e7914398c582266974670a2a9755904
Diogo Carreira <dcweb19@gmail.com>
2024-07-15 13:48:27 +0100

    correction

--- logical_operators.py
+++ logical_operators.py
@@ -7,7 +7,7 @@ is_expert = True
 
 if is_magician and is_expert:
     print('You are a master magician')
-elif is_magician:
+elif is_magician and not is_expert:
     print('At least you are getting there')
 else:
     print('You need magic powers')
