e2fce998fa1aead399423b608e2d41c4b0f2a1f5
Diogo Carreira <dcweb19@gmail.com>
2024-08-01 09:05:40 +0100

    exercicios corrigido

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,10 @@
 #Exercise: check for duplicates in list
 
 some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
-duplicate = []
+duplicates = []
 
-if some_list == duplicate
+for values in some_list:
+    if some_list.count(values)>1:
+        if values not in duplicates:
+            duplicates.append(values)
+print(duplicates)
