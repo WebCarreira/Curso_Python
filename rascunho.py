0eef50fdd3390f0f2bf72f90ad68ae97481655ed
Diogo Carreira <dcweb19@gmail.com>
2024-08-06 18:05:47 +0100

    rascunho1

--- rascunho.py
+++ rascunho.py
@@ -1,1 +1,15 @@
-'hello'.capitalize()
+#def power(x,n):
+#    if n==0:
+#        return 1
+#    else:
+#        return x*power(x,n-1)
+#print(power(2,3))
+
+#x = 10
+#y = 4
+# z = x // y #dá erro porque tem espaçamento a mais
+#print(z)
+
+x = {1, 2, 3}
+x.add(2)
+print(x)
